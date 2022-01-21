7cebd1508d7a2980fcaf947633a4904d56967441
TaehoLEEKR <80689135+TaehoLEEKR@users.noreply.github.com>
2022-01-21 15:07:20 +0900

    Delete main directory

--- main/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Questions , Choice , Type_Pattern , MBTI_Res ,Result
-
-admin.site.register(Questions)
-admin.site.register(Choice)
-admin.site.register(Type_Pattern)
-admin.site.register(MBTI_Res)
-admin.site.register(Result)
