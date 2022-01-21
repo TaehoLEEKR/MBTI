f43c531adb7583d7a41825c1998b9943a89f0910
이대호 <679748@naver.com>
2022-01-21 07:56:25 +0000

    MBTI_index_Counting

--- main/views.py
+++ main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Questions, Choice, Type_Pattern, MBTI_Res, Result
 
-# Create your views here.
+def index(request):
+    mbti_res =MBTI_Res.objects.all() # MBTI 유형 16가지 데이터 전부 가져오는 함수
+    Cnt_mb = 0 # 메인페이지 index 에서 여태 참여한 MBTI 유형 카운팅 수
+    for i in range(1,len(mbti_res)+1):
+        Cnt_mb += MBTI_Res.objects.get(pk=i).Cnt # 모든 MBTI_Res 에서 pk가 i번째에 있는 Cnt 변수를 모두 카운팅 한다.
+    
+    context = {
+        'Cnt_mb' : Cnt_mb,
+    }
+    
+    return render(request,'index.html',context)
